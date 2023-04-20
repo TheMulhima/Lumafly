278dc88f847ea7ad3656b0d7ef972e8a7efd9d03
Mulhima <mulhima003@gmail.com>
2023-04-20 10:35:41 -0400

    Use MenuItems ContextFlyout for BulkActions Flyout
    Instead of adding an Attached Flyout and trying to open it properly

--- Scarab/Views/ModListView.xaml.cs
+++ Scarab/Views/ModListView.xaml.cs
@@ -1,11 +1,13 @@
 using System;
-using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Primitives.PopupPositioning;
 using Avalonia.Input;
+using Avalonia.LogicalTree;
 using Avalonia.Markup.Xaml;
-using Avalonia.Media;
 using JetBrains.Annotations;
 using Scarab.ViewModels;
 
@@ -16,7 +18,6 @@ namespace Scarab.Views
     {
         private readonly TextBox _search;
         private readonly MenuItem _bulkActions;
-        private readonly Stopwatch _bulkActionsMenuStopwatch = new();
 
         public ModListView()
         {
@@ -28,39 +29,28 @@ namespace Scarab.Views
             _bulkActions = this.FindControl<MenuItem>("BulkActions");
         }
 
-        [UsedImplicitly]
-        private void BulkActionsFlyoutOpened(object? sender, EventArgs e)
+        // MenuItem's Popup is not created when ctor is run. I randomly overrided methods until
+        // I found one that is called after Popup is created. There is nothing special about ArrangeCore
+        protected override void ArrangeCore(Rect finalRect)
         {
-            _bulkActions.Background = SolidColorBrush.Parse("#505050");
+            base.ArrangeCore(finalRect);
+            SetUpBulkActionsPopup();
         }
-        
-        [UsedImplicitly]
-        private void BulkActionsFlyoutClosed(object? sender, EventArgs e)
-        {
-            _bulkActions.Background = null;
-            _bulkActionsMenuStopwatch.Start();
-        }
-        
-        [UsedImplicitly]
-        private void OpenBulkActionsMenu(object? sender, PointerPressedEventArgs e)
-        {
-            // any pointer pressed event outside the flyout will close it (including this button)
-            // so if a close did happen less than 100ms before, then dont open it. 
-            // if it causes problems, it can be reduced to 10 ms
-            if (_bulkActionsMenuStopwatch.IsRunning)
-            {
-                _bulkActionsMenuStopwatch.Stop();
-                var lastClosedTime = _bulkActionsMenuStopwatch.ElapsedMilliseconds;
-                _bulkActionsMenuStopwatch.Reset();
-                    
-                if (lastClosedTime < 100)
-                {
-                    return;
-                }
-            }
 
-            FlyoutBase.ShowAttachedFlyout((sender as Control)!);
+        // I havent found a way to set these properties normally
+        private void SetUpBulkActionsPopup()
+        {
+            var menuItem_popup = typeof(MenuItem).GetField("_popup", BindingFlags.Instance | BindingFlags.NonPublic)!.GetValue(_bulkActions);
+
+            var popup = menuItem_popup as Popup ?? throw new Exception("Bulk Actions popup not found");
+            
+            popup.PlacementMode = PlacementMode.Right;
+            popup.PlacementAnchor = PopupAnchor.TopRight;
+            popup.PlacementGravity = PopupGravity.TopRight;
+            popup.OverlayDismissEventPassThrough = true;
+            popup.IsLightDismissEnabled = true;
         }
+        
         private void OnKeyDown(object? sender, KeyEventArgs e)
         {
             if (!_search.IsFocused)
