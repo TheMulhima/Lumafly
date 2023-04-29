f22f57a19badc02401e24ba5562e621be428bac1
Mulhima <mulhima003@gmail.com>
2023-04-29 12:55:49 -0400

    show better error message on io errors
    - now handles write protected and file locked

--- Scarab/Util/DisplayErrors.cs
+++ Scarab/Util/DisplayErrors.cs
@@ -106,38 +106,89 @@ public static class DisplayErrors
             await action();
         }
     }
+
+    private enum IOErrorType
+    {
+        LockedFile, 
+        WriteProtected,
+        Other
+    }
     
     public static async Task HandleIOExceptionWhenDownloading(ModItem item, Exception e, string action)
     {
-        string additionalText = "";
-        string filePath = e.Message.Split('\'')[1];
-        List<Process> processes = new List<Process>();
-        string listOfProcesses = "";
-        if (OperatingSystem.IsWindows())
+        var errorType = e switch
         {
-            try
-            {
-                processes = FileAccessLookup.WhoIsLocking(filePath);
-                if (processes.Count > 0)
-                {
-                    listOfProcesses = $"{string.Join("\n-", processes.Select(x => x.ProcessName))}";
-                    Trace.WriteLine($"Following processes is locking the file {listOfProcesses}");
-                    additionalText =
-                        $"\nPlease close the following processes as they are locking important files:\n{listOfProcesses}";
-                }
-            }
-            catch (Exception)
-            {
-                //ignored as its not a requirement
-            }
-        }
+            _ when e.Message.Contains("The process cannot access the file") => IOErrorType.LockedFile,
+            _ when e.Message.Contains("The media is write protected") => IOErrorType.WriteProtected,
+            _ => IOErrorType.Other
+        };
+        
+        string additionalText = "";
+
+        additionalText = errorType switch
+        {
+            IOErrorType.LockedFile => GiveMoreDetailsOnLockedFileError(e),
+            IOErrorType.WriteProtected => GiveMoreDetailsOnWriteProtectedError(e),
+            _ => ""
+        };
 
         item.CallOnPropertyChanged(nameof(ModItem.InstallingButtonAccessible));
 
         await DisplayGenericError(
             $"Unable to {action} {item.Name}.\n" +
             $"Scarab was unable to access the file in the mods folder.\n" +
-            $"Please make sure to close all other apps that could be using the mods folder\n" +
             additionalText, e);
+    }
+
+    private static string GetFileNameFromError(string message, string start)
+    {
+        int indexOfErrorStart = message.IndexOf(start, StringComparison.Ordinal);
+        string filePath = message[indexOfErrorStart..].Split('\'')[1];
+        return filePath;
+    }
+
+    private static string GiveMoreDetailsOnWriteProtectedError(Exception e)
+    {
+        var additionalText = $"Please make sure that the mods folder is not in a write protected location.\n";
+
+        try
+        {
+            string file = GetFileNameFromError(e.Message, "The media is write protected.");
+            additionalText += $"Scarab cannot access the file: {file}";
+        }
+        catch (Exception)
+        {
+            // ignored as its not a requirement
+        }
+
+        return additionalText;
+    }
+    
+    private static string GiveMoreDetailsOnLockedFileError(Exception e)
+    {
+        var additionalText = "Please make sure to close all other apps that could be using the mods folder\n";
+        try
+        {
+            string filePath = GetFileNameFromError(e.Message, "The process cannot access the file");
+            if (OperatingSystem.IsWindows())
+            {
+
+                var processes = FileAccessLookup.WhoIsLocking(filePath);
+                if (processes.Count > 0)
+                {
+                    var listOfProcesses = $"{string.Join("\n-", processes.Select(x => x.ProcessName))}";
+                    Trace.WriteLine($"Following processes is locking the file {listOfProcesses}");
+                    additionalText +=
+                        $"\nPlease close the following processes as they are locking important files:\n{listOfProcesses}";
+                }
+
+            }
+        }
+        catch (Exception)
+        {
+            //ignored as its not a requirement
+        }
+
+        return additionalText;
     }
 }
