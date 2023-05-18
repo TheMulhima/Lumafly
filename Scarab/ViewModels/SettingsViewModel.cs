4332b7b04f73a124d8aba4a4c1e68e9c24e1b8a9
Mulhima <81007262+TheMulhima@users.noreply.github.com>
2023-05-19 01:34:30 +0400

    Revert "open config folder"
    This reverts commit f1a99b129369f5ecc85673ef140c6a4c1c7aec13.

--- Scarab/ViewModels/SettingsViewModel.cs
+++ Scarab/ViewModels/SettingsViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Reactive;
 using System.Threading.Tasks;
 using Avalonia;
@@ -105,9 +104,6 @@ namespace Scarab.ViewModels
 
         private async Task ChangePathAsync()
         {
-            Process.Start(new ProcessStartInfo(Settings.ConfigFolderPath) {
-                UseShellExecute = true
-            });
             string? path = await PathUtil.SelectPathFallible();
 
             if (path is null)
