5e753d7e7ab2a4695352868ac203dee9f4cbdc75
Mulhima <mulhima003@gmail.com>
2023-06-12 00:58:20 +0400

    fix command in publish.bat
    - make it copy the exe for easier testing
    - make the delete command in the correct format
    - dont include extra dir in zip

--- publish.bat
+++ publish.bat
@@ -1,13 +1,15 @@
 cd Scarab
 
- dotnet publish -r win-x64 -p:PublishSingleFile=true -p:Configuration=Release --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugType=embedded
+dotnet publish -r win-x64 -p:PublishSingleFile=true -p:Configuration=Release --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugType=embedded
 dotnet publish -r linux-x64 -p:Configuration=Release -p:PublishSingleFile=true -p:UseAppHost=true --self-contained true
 dotnet publish -r osx-x64 -p:Configuration=Release -p:PublishSingleFile=true -p:UseAppHost=true --self-contained true
 
 cd ..
 
-rm out/*.zip
+del /Q /F "%~dp0out\*"
+
+copy "%~dp0Scarab\bin\Release\net7.0\win-x64\publish\Scarab.exe" "%~dp0out\Scarab.exe"
 
 python make_app.py Scarab.app Scarab/bin/Release/net7.0/osx-x64/publish
-7z a "out/windows.zip" "./Scarab/bin/Release/net7.0/win-x64/publish/"
-7z a "out/linux.zip" "./Scarab/bin/Release/net7.0/linux-x64/publish/"
+7z a "out/windows.zip" "./Scarab/bin/Release/net7.0/win-x64/publish/*"
+7z a "out/linux.zip" "./Scarab/bin/Release/net7.0/linux-x64/publish/*"
