d94d57ffdf9a6ced9270557a9279d3ded387b01e
Mulhima <mulhima003@gmail.com>
2023-07-10 00:27:30 +0400

    add issues field in tests

--- Scarab.Tests/DatabaseTest.cs
+++ Scarab.Tests/DatabaseTest.cs
@@ -114,6 +114,7 @@ namespace Scarab.Tests
                     "QoL",
                     "A collection of various quality of life improvements.",
                     "https://github.com/fifty-six/HollowKnight.QoL",
+                    "https://github.com/fifty-six/HollowKnight.QoL",
                     Array.Empty<string>(),
                     Array.Empty<string>(),
                     Array.Empty<string>()
