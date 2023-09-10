c8059c5c19612224e6f42d6d0b9d3521c153b887
Mulhima <mulhima003@gmail.com>
2023-09-11 00:21:50 +0400

    fix issues with edit pack window
    - make startup location center owner
    - make it size fixed to main window size
    - force dismiss the popup of selected mods

--- Scarab/Views/Windows/EditPackWindow.axaml.cs
+++ Scarab/Views/Windows/EditPackWindow.axaml.cs
@@ -1,5 +1,12 @@
-﻿using Avalonia.Controls;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
+using Avalonia.Threading;
+using Scarab.Util;
 
 namespace Scarab.Views.Windows;
 
@@ -8,6 +15,10 @@ public partial class EditPackWindow : Window
     public EditPackWindow()
     {
         InitializeComponent();
+
+        var mainWindow = AvaloniaUtils.GetMainWindow();
+        Width = mainWindow.Width;
+        Height = mainWindow.Height;
     }
 
     private void RequestSave(object? sender, RoutedEventArgs e)
@@ -18,5 +29,26 @@ public partial class EditPackWindow : Window
     private void RequestCancel(object? sender, RoutedEventArgs e)
     {
         Close(false);
+    }
+
+    private void DismissOnClick(object? sender, RoutedEventArgs e)
+    {
+
+        Dispatcher.UIThread.InvokeAsync(async () =>
+        {
+            
+            if (sender is not ILogical button) return;
+            var stackPanel = button.GetLogicalParent();
+            var flyoutPresenter = stackPanel?.GetLogicalParent();
+            var popup = flyoutPresenter?.GetLogicalParent() as Popup;
+            
+            await Task.Delay(100);
+
+            popup?.Close();
+            
+            await Task.Delay(100);
+            
+            popup?.Close();
+        });
     }
 }
